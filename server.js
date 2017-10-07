f8a6b0817ddd76d36a3355f892535a9778ae1c1b
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-10-06 17:31:56 -0700

    adding reading image names from file system

--- server.js
+++ server.js
@@ -2,11 +2,25 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+app.get('/images', (req, res) => {
+  const imgFolder = __dirname + '/public/images/';
+  const fs = require('fs');
+  fs.readdir(imgFolder, (err, files) => {
+    if (err) {
+      return console.log('we ran into error: ', err);
+    }
+    const filesArr = [];
+    files.forEach((file) => {
+      filesArr.push({ name: file });
+    });
+    res.json(filesArr);
+  })
 });
 
 app.listen(3000, function() {
